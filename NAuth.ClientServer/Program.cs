a740b5a24df306f3ae3ecade76e1ac6ee6836131
lkc311 <lkc311@163.com>
2018-05-13 10:05:47 +0800

    add server

---
+++ NAuth.ClientServer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NAuth.ClientServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
